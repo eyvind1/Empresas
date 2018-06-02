f05642cd0b979280f9ecc989ad06c6bd4cff024c
eyvind <eyvind@localhost.localdomain>
2018-06-02 10:28:21 -0500

    subiendo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,67 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { DestinosComponent } from './componentes/destinos/destinos.component';
+import { AlojamientosComponent } from './componentes/alojamientos/alojamientos.component';
+import { RestaurantsComponent } from './componentes/restaurants/restaurants.component';
+import { ContactenosComponent } from './componentes/contactenos/contactenos.component';
+import { LoginComponent } from './componentes/login/login.component';
+
+
+const appRoutes : Routes = [
+  {
+    path: "",
+    component: LoginComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  
+  {
+    path:"contactenos",
+    component: ContactenosComponent
+  },
+  {
+    path:"destinos",
+    component: DestinosComponent
+  },
+  {
+    path:"alojamientos",
+    component: AlojamientosComponent
+  },
+  {
+    path:"restaurantes",
+    component: RestaurantsComponent
+  },
+  {
+    path:"home",
+    component: HomeComponent
+  },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    DestinosComponent,
+    AlojamientosComponent,
+    RestaurantsComponent,
+    ContactenosComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
